a37d7fdb8285dfdb266f9f73df6947cc9b9c39af
Mezbaul Haque <mezbaul.cs@gmail.com>
2024-08-16 20:25:21 +0100

    Update

--- mica/__init__.py
+++ mica/__init__.py
@@ -189,12 +189,10 @@ def main():
         #     report_file=settings.PACKAGE_ROOT_DIR / ".." / "output" / f"report_ho_P{sub_no:03d}.json",
         # )
 
-        time.sleep(0.5)
-
     for train_fold, test_fold in KFold(n_splits=len(sub_nums)).split(sub_nums):
         sub_no = sub_nums[test_fold[0]]
 
-        if sub_no in [1, 2]:
+        if sub_no not in [7, 8, 9]:
             continue
 
         test_data = all_test_sets[test_fold[0]]
