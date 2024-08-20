736148254b9a90f35f9a4568feb2a08d9830d34d
mezbaul-h <11481344+mezbaul-h@users.noreply.github.com>
2024-08-20 18:49:39 +0100

    Update __init__.py

--- mica/__init__.py
+++ mica/__init__.py
@@ -194,7 +194,7 @@ def main():
     for train_fold, test_fold in KFold(n_splits=len(sub_nums)).split(sub_nums):
         sub_no = sub_nums[test_fold[0]]
 
-        if sub_no not in [16, 17, 18]:
+        if sub_no not in [19, 20]:
             continue
 
         print("Subject", sub_no)
